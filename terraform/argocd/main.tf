6e0b36f95dc57f18d27bc1f671e650873d2eb8a4
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-04-20 20:54:17 +0530

    Update main.tf

--- terraform/argocd/main.tf
+++ terraform/argocd/main.tf
@@ -1,16 +1,21 @@
-provider "helm" { 
+provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
 
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = var.argocd_version
-
+  name             = "argocd"
+  chart            = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  namespace        = "argocd"
   create_namespace = true
-  values = [file("${path.module}/argocd-values.yaml")]
+  version          = "5.46.6"
+
+  force_update     = true        # Ensures Helm release is updated if drift is detected
+  recreate_pods    = true        # Restarts pods on update
+  timeout          = 600         # Wait time for deployment
+
+  # Optional: Use your existing values file
+  # values = [ file("argocd-values.yaml") ]
 }
